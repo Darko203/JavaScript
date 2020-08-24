6d7357f62f2ca8a88a62c9fbb65facc01db5aa6b
Darko <67801603+Darko203@users.noreply.github.com>
2020-08-24 16:23:02 +0200

    prva domasna rabota

---
+++ 01.js
@@ -0,0 +1,79 @@
+// •Да се напише програма која при внесен произволен број од тастатура ќе испечати на екран дали е позитивен, негативен или нула
+let broj = prompt("Vnesi broj");
+if (!isNaN(broj) && broj > 0) {
+  console.log("Pozitiven broj");
+} else if (!isNaN(broj) && broj < 0) {
+  console.log("Negativen broj");
+} else if (!isNaN(broj) && broj == 0) {
+  console.log("Nula");
+} else {
+  console.log("Nevalidna forma");
+}
+
+// •Да се напише програма која ќе го отпечати максимумот од два броја чии вредности се внесуваат од тастатура
+let broj1 = prompt("vnesi broj1");
+let broj2 = prompt("vnesi broj2");
+if (broj1 > broj2) {
+  console.log(broj1);
+} else {
+  console.log(broj2);
+}
+
+// Да се напише програма која проверува дали дадена година која се вчитува од тастатура е престапна или не и на екран ќе отпечати соодветна порака
+let godina = 2019;
+function isLeap(year) {
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
+        return "Leap year.";
+      } else {
+        return "Not leap year.";
+      }
+    } else {
+      return "Leap year";
+    }
+  } else {
+    return "Not leap year.";
+  }
+}
+// Од тастатура се внесуваат координати на една точка.Да се напише програма со која ќе се испечати на кој квадрант или оска припаѓа внесената точка.Ако станува збор за точка која лежи на координатниот почеток, да се испечати соодветна порака.
+let xOska = prompt("Vnesi koordinati na x-oska");
+let yOska = prompt("Vnesi koordinati po y-oska");
+if (xOska > 0 && yOska > 0) {
+  console.log("Kvadrant1");
+} else if (xOska < 0 && yOska > 0) {
+  console.log("Kvadrant2");
+} else if (xOska < 0 && yOska < 0) {
+  console.log("Kvadrant3");
+} else if (xOska > 0 && yOska < 0) {
+  console.log("Kvadrant4");
+} else if (xOska == 0 && yOska == 0) {
+  console.log("Tockata lezi na koordinatniot pocetok");
+}
+// •Да се напише програма која ќе претставува едноставен калкулатор.Во програмата се вчитуваат два броја и оператор
+let broj1 = 5;
+let broj2 = 7;
+let operator = prompt("Vnesi operator");
+if (!isNaN(broj1) && !isNaN(broj2)) {
+  switch (operator) {
+    case "+":
+      console.log(broj1 + broj2);
+      break;
+    case "-":
+      console.log(broj1 - broj2);
+      break;
+    default:
+      console.log("Vnesovte pogresen operator");
+  }
+}
+// Од тастатура се внесуваат три броја кои не мора да се подредени.Внесените броеви претставуваат должини на страните на правоаголен триаголник.Да се напише програма која ќе проверува дали може да се конструира триаголник од дадените должини, при што ако може, треба да се провери дали истиот е правоаголен и да се пресмета неговата плоштина.Во спротивно, треба да се испечати порака дека триаголникот не е правоаголен
+let a = 3;
+let b = 4;
+let c = 5;
+let niz = [a, b, c];
+console.log(niz.sort());
+if (a * a + b * b == c * c) {
+  console.log("Pravoagolen triagolnik");
+} else {
+  console.log("Triagolnikot ne e pravoagolen");
+}
